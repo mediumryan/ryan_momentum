25bd87caf5d0a23dce77467f5c30e3260f4366b9
Ryan <straightwind0726@gmail.com>
2023-06-16 15:50:05 +0900

    [Update] compress bg image files

--- JS/bg.js
+++ JS/bg.js
@@ -2,22 +2,21 @@ const container = document.getElementById("main_container");
 
 // 이미지 파일 배열
 const imgArr = [
-  "https://mediumryan.github.io/momemtum_clone/Images/1.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/2.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/3.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/4.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/5.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/6.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/7.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/8.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/9.jpg",
-  "https://mediumryan.github.io/momemtum_clone/Images/10.jpg",
+  "/Images/1.jpg",
+  "/Images/2.jpg",
+  "/Images/3.jpg",
+  "/Images/4.jpg",
+  "/Images/5.jpg",
+  "/Images/6.jpg",
+  "/Images/7.jpg",
+  "/Images/8.jpg",
+  "/Images/9.jpg",
+  "/Images/10.jpg",
 ];
 
 // 랜덤 이미지 파일 경로 얻기
 function getRandomImagePath(arr) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
+  return arr[(randomIndex = Math.floor(Math.random() * arr.length))];
 }
 
 // 배경화면에 랜덤 이미지 적용
