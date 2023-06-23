2dfc8d8f7ba401e723ef3ff542a2350f683cbd7c
Ryan <straightwind0726@gmail.com>
2023-06-23 12:00:16 +0900

    [Update] update code from js to jquery bg,clock,quotes section

--- JS/clock.js
+++ JS/clock.js
@@ -1,10 +1,14 @@
-const clock = document.getElementById('clock_container');
+$(document).ready(function () {
+  const clock = $("#clock_container");
 
-function getCurrentTime() {
-  const currentDate = new Date();
-  const currentTimeString = currentDate.toLocaleTimeString("ko-KR");
-  clock.innerText = currentTimeString;
-}
+  // Date() 메소드를 이용해서 현지 시간 가져오기
+  function getCurrentTime() {
+    const currentDate = new Date();
+    const currentTimeString = currentDate.toLocaleTimeString("ko-KR");
+    clock.text(currentTimeString);
+  }
 
-getCurrentTime();
-const currentTime = setInterval(getCurrentTime, 1000);
+  // 1초에 한번씩 시간 변경
+  getCurrentTime();
+  const currentTime = setInterval(getCurrentTime, 1000);
+});
--- JS/weather.js
+++ JS/weather.js
@@ -1,32 +1,34 @@
-const weatherLocation = document.querySelector('.weather_location');
-const temp = document.querySelector('.weather_temp');
-const weather = document.querySelector('.weather_weather');
+const weatherLocation = document.querySelector(".weather_location");
+const temp = document.querySelector(".weather_temp");
+const weather = document.querySelector(".weather_weather");
 
 // API 데이터 불러오기
 function callDatas(lat, lon) {
-  const API_KEY = '30a223157b4e106a3d4631fa76a27b4a';
+  const API_KEY = "30a223157b4e106a3d4631fa76a27b4a";
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
-        console.log(response);
         return response.json();
       }
-      throw new Error('Network response was not ok.');
+      throw new Error("Network response was not ok.");
     })
-    .then(data => {
+    .then((data) => {
       // 불러온 데이터를 처리하는 코드 작성
       weatherLocation.innerText = data.name;
       temp.innerText = `${Math.floor(data.main.temp)} °C`;
       weather.innerText = data.weather[0].main;
     })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
     });
 }
 
-// 사용자의 위도, 경도값 받아오기 
+// 사용자의 위도, 경도값 받아오기
 function success(pos) {
   const crd = pos.coords;
 
@@ -38,7 +40,7 @@ function success(pos) {
 
   // 이후 30초마다 호출
   setInterval(() => {
-    callDatas(lat,lon);
+    callDatas(lat, lon);
   }, 30000);
 }
 
