a096ef5bd77f3af6338abe66133bf06e5aa987d6
Ryan <straightwind0726@gmail.com>
2023-09-26 16:57:33 +0900

    [Update] Change some category styles

--- src/Components/todo/TodoLeft/AddTodo.jsx
+++ src/Components/todo/TodoLeft/AddTodo.jsx
@@ -8,6 +8,7 @@ export const AddsWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    width: 100%;
 `;
 
 export const AddsLabel = styled.label`
--- src/Components/todo/TodoLeft/Categories.jsx
+++ src/Components/todo/TodoLeft/Categories.jsx
@@ -2,19 +2,35 @@ import { styled } from 'styled-components';
 import { AddsLabel, AddsWrapper } from './AddTodo';
 import { useRecoilState } from 'recoil';
 import { categoryState } from '../../../atom';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const CategoryBox = styled.div`
+    width: 75%;
+    max-height: 120px;
+    overflow-y: scroll;
     border: 2px solid var(--text-100);
     border-radius: 10px;
     padding: var(--padding-double-medium);
 `;
 
 const Category = styled.button`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     color: var(--text-100);
     font-size: var(--font-size-micro);
-    margin: 0 var(--margin-small);
-    &:hover {
-        color: var(--primary-100);
+    margin: var(--margin-small) 0;
+    span {
+        &:hover {
+            color: var(--primary-200);
+        }
+    }
+    svg {
+        margin-left: var(--margin-small);
+        &:hover {
+            color: var(--primary-100);
+        }
     }
 `;
 
@@ -32,13 +48,13 @@ export default function Categories() {
             <CategoryBox>
                 {category.map((cat, index) => {
                     return (
-                        <Category
-                            key={index}
-                            onClick={() => {
-                                deleteCategory(cat);
-                            }}
-                        >
-                            {cat}
+                        <Category key={index}>
+                            <span>{cat}</span>
+                            <FaTrashAlt
+                                onClick={() => {
+                                    deleteCategory(cat);
+                                }}
+                            />
                         </Category>
                     );
                 })}
